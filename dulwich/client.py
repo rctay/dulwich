2c73fee6cf5df31100f8bd6ff2ade8e9a688ad47
Tay Ray Chuan <rctay89@gmail.com>
2010-03-29 11:52:47 +0800

    dulwich.client: handle sideband changes in git
    Since git v1.7.0.2 [1], a packet-flush is emitted following the pack
    data if sideband capability is advertised. Include this packet flush
    ('0000') in the accepted values.
    
    [1] 38a81b4 (receive-pack: Wrap status reports inside side-band-64k)

--- dulwich/client.py
+++ dulwich/client.py
@@ -120,7 +120,7 @@ class GitClient(object):
 
         # read the final confirmation sha
         client_sha = self.proto.read(20)
-        if not client_sha in (None, "", sha):
+        if not client_sha in (None, "", sha, "0000"):
             raise ChecksumMismatch(sha, client_sha)
 
         return new_refs

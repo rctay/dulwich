0734ee5e4041195a2fc1b053ef428da059e5e087
Tay Ray Chuan <rctay89@gmail.com>
2010-07-10 12:46:13 +0800

    test_repository.DiskRefsContainerTests.test_remove_packed_without_peeled: prevent '\r' on windows
    By writing to the file in binary mode, we stop python from munging the
    packed-refs file with '\r'-style line endings on Windows.

--- dulwich/tests/test_repository.py
+++ dulwich/tests/test_repository.py
@@ -740,7 +740,7 @@ class DiskRefsContainerTests(RefsContainerTests, unittest.TestCase):
         f = open(refs_file)
         refs_data = f.read()
         f.close()
-        f = open(refs_file, 'w')
+        f = open(refs_file, 'wb')
         f.write('\n'.join(l for l in refs_data.split('\n')
                           if not l or l[0] not in '#^'))
         f.close()
